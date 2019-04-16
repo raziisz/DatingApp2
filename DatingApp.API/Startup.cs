28e1c6e6ef4055acdc6615e5176a109208b86fb5
Luiz Felipe <raziel_libertino@hotmail.com>
2019-04-16 11:54:59 -0400

    End Section of 13

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -59,6 +59,7 @@ namespace DatingApp.API
                     ValidateAudience = false
                 };
             });
+            services.AddScoped<LogUserActivity>();
            
 
         }
